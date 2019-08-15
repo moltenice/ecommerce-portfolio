483a8ddf7a7d6e33d96e574bf82875db9e3d947e
Karan Shah <dabrainstabber@gmail.com>
2019-08-15 22:45:18 +0100

    Added a fun little transition for the website logo

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './logo.styles.scss';
 
-const Logo = () => {
-    return <h1>Portfolio E-commerce Website</h1>;
-};
+const Logo = () => (
+    <h1 className='logo'>
+        P<span className='extended-text'>ortfolio </span>
+        E<span className='extended-text'>-commerce </span>
+        W<span className='extended-text'>ebsite</span>
+    </h1>
+);
 
 export default Logo;
