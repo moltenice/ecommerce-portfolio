edd643275d996d9c81164b34f950b461c2cdc81d
Karan Shah <dabrainstabber@gmail.com>
2019-08-20 00:15:56 +0100

    Storing user authentication info into the database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:209413756285:web:e6dfb0cb92b5ae00"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (userAuth) {
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+        const snapshot = await userRef.get();
+
+        if (!snapshot.exists) {
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+
+            try {
+                await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                });
+            } catch (error) {
+                console.log('error creating user', error.message);
+            }
+        }
+
+        return userRef;
+    }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
