fbf26b02ef74b024c22593aaeef26763dcea51bb
Karan Shah <dabrainstabber@gmail.com>
2019-09-28 11:23:02 +0100

    Fix failing test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
@@ -49,14 +49,16 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Header currentUser={this.state.currentUser} />
-                <Switch>
-                    <Route exact path='/' component={HomePage} />
-                    <Route path='/shop' component={ShopPage} />
-                    <Route path='/signin' component={SignInAndSignUpPage} />
-                </Switch>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Header currentUser={this.state.currentUser} />
+                    <Switch>
+                        <Route exact path='/' component={HomePage} />
+                        <Route path='/shop' component={ShopPage} />
+                        <Route path='/signin' component={SignInAndSignUpPage} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
